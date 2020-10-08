62ff158143e5545f5f3d6066eaff059a606dc853
singhrobin123 <chaudharyrobin199@gmail.com>
2020-10-08 17:08:05 +0530

    forth2 first commit

---
+++ client/build/precache-manifest.14efb6c8177be04a1580cbfab111818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a9994cecd282f36333",
+    "url": "/static/css/main.6bbdbcf6.chunk.css"
+  },
+  {
+    "revision": "f7a9994cecd282f36333",
+    "url": "/static/js/main.f7a9994c.chunk.js"
+  },
+  {
+    "revision": "a9d5ca7dde8f3c7f563c",
+    "url": "/static/css/1.53d90aab.chunk.css"
+  },
+  {
+    "revision": "a9d5ca7dde8f3c7f563c",
+    "url": "/static/js/1.a9d5ca7d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f282bf8695c3f69cc1f814634e7e5371",
+    "url": "/index.html"
+  }
+];
